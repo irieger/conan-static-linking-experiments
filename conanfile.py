982760f9e50cf8cceb3391b86ba96cd8ab02d10b
Ingmar Rieger <git@irieger.net>
2021-11-06 12:29:17 +0100

    Current build state, M1 global weak symbol problem

--- conanfile.py
+++ conanfile.py
@@ -2,10 +2,24 @@ from conans import ConanFile, CMake
 
 class TestDependenciesConan(ConanFile):
     settings = 'cppstd', 'os', 'compiler', 'build_type', 'arch'
-    requires = 'gflags/2.2.2', 'glog/0.4.0', 'yaml-cpp/0.6.3', 'eigen/3.3.8', 'ceres-solver/2.0.0', 'openimageio/2.2.7.0'
+    requires = (
+        'gflags/2.2.2',
+        'glog/0.4.0',
+        'yaml-cpp/0.6.3',
+        'eigen/3.3.8',
+        'ceres-solver/2.0.0',
+        'opencolorio/2.1.0',
+        'openimageio/2.3.7.2'
+    )
     generators = 'cmake'
-    default_options = {'gflags:shared': False, 'glog:shared': False, 'yaml-cpp:shared': False, 'eigen:MPL2_only': True, 'ceres-solver:shared': False,
-        'libtiff:jbig': False}
+    default_options = {
+        '*:shared': False,
+        '*:visibility': 'hidden',
+        'eigen:MPL2_only': True,
+        'ceres-solver:use_eigen_sparse': False,
+        'libtiff:jbig': False,
+        'openimageio:with_ffmpeg': False, 'openimageio:with_ptex': False, 'openimageio:with_opencv': False
+    }
 
     def build(self):
         cmake = CMake(self)
